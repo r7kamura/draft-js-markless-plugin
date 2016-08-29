36f737cf1cbfd7c838b7c2d4b50de55164c2ea7a
r7kamura <r7kamura@gmail.com>
2016-08-29 16:58:08 +0900

    Put editor on demo page

--- demo/application.js
+++ demo/application.js
@@ -1,12 +1,32 @@
-import CreateMarklessPlugin from "../src/index.js";
+import { EditorState } from "draft-js";
+import createMarklessPlugin from "../src/index.js";
+import Editor from "draft-js-plugins-editor";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const marklessPlugin = createMarklessPlugin();
+const plugins = [
+  marklessPlugin,
+];
+
 class Root extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { editorState: EditorState.createEmpty() };
+  }
+
+  onChange(editorState) {
+    this.setState({ editorState });
+  }
+
   render() {
     return(
       <div>
-        root
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange.bind(this)}
+          plugins={plugins}
+        />
       </div>
     );
   }
