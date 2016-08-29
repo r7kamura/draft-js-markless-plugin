4dd6356a90792c7f76177450731f993bc662289f
r7kamura <r7kamura@gmail.com>
2016-08-29 17:45:58 +0900

    Add some community plugins for demo use

--- demo/application.js
+++ demo/application.js
@@ -1,11 +1,30 @@
 import { EditorState } from "draft-js";
+import createAutoListPlugin from "draft-js-autolist-plugin";
+import createBlockBreakoutPlugin from "draft-js-block-breakout-plugin";
+import createLinkifyPlugin from "draft-js-linkify-plugin";
 import createMarklessPlugin from "../src/index.js";
 import Editor from "draft-js-plugins-editor";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const autoListPlugin = createAutoListPlugin();
+const blockBreakoutPlugin = createBlockBreakoutPlugin({
+  breakoutBlocks: [
+    "blockquote",
+    "header-five",
+    "header-four",
+    "header-one",
+    "header-six",
+    "header-three",
+    "header-two",
+  ]
+});
+const linkifyPlugin = createLinkifyPlugin();
 const marklessPlugin = createMarklessPlugin();
 const plugins = [
+  autoListPlugin,
+  blockBreakoutPlugin,
+  linkifyPlugin,
   marklessPlugin,
 ];
 
